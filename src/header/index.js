0758e66cd2cebf5b9817c856ad9033eec1708bcc
Kajol Tale <kajoltale21@gmail.com>
2019-06-07 17:38:17 +0530

    Merge branch 'master' of https://github.com/Saga09/chtrbox
    # Conflicts:
    #	src/brand/card.js

--- src/header/index.js
+++ src/header/index.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import {HashRouter, Route, Switch, Redirect} from 'react-router-dom';
 import Profile from './Profile'
-import BrandCards from '../brand/card';
+import BrandCards from '../brand/index';
 // require('bootstrap-material-design')
 
 class Header extends React.Component{
     render(){
         return(
             <div>
-                <HashRouter>
-                        <Switch>
-                            <Route path="/dashboard/:activeTab?" component={Profile}/>
-                            <Route path="/brand/" component={BrandCards}/>
-                            {/*<Redirect to="/brand"/>*/}
-
-                        </Switch>
-                </HashRouter>
-
                 <ul className="nav nav-tabs">
                     <li className="nav-item">
                         <a className="nav-link active" href="#">Search</a>
@@ -28,6 +19,16 @@ class Header extends React.Component{
                         <a className="nav-link" href="#">Login</a>
                     </li>
                 </ul>
+                <HashRouter>
+                        <Switch>
+                            <Route path="/dashboard/:activeTab?" component={Profile}/>
+                            <Route path="/brand/" component={BrandCards}/>
+                            {/*<Redirect to="/brand"/>*/}
+
+                        </Switch>
+                </HashRouter>
+
+
             </div>
         )
     }
