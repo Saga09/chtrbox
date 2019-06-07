0b4c43177722b01269d60d683b44510e527644bb
Sagar <sagar@chtrbox.com>
2019-06-07 16:14:10 +0530

    # mycommint

--- src/brand/card.js
+++ src/brand/card.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Profiles from '../profiles.json';
 
 const cardDetail = [
     {'name':'Stephen Kramer Glickman', 'type': 'Comedian - Actor - Podcaster', 'url': 'https://d31wcbk3iidrjq.cloudfront.net/avatar-s2FSIyKyi.jpg'},
@@ -11,15 +12,39 @@ const cardDetail = [
 
 class Card extends React.Component{
 
+    safeReturn(object,index,defaultValue)
+    {
+        defaultValue = defaultValue == undefined ? false : defaultValue;
+        if(object == undefined || index == undefined)
+        {
+            return defaultValue;
+        }
+        if(object[index] == undefined || object[index] === '')
+        {
+            return defaultValue;
+        }
+        return object[index];
+    }
+
     getCardDetail(){
         let cardXml = []
 
-        for(let i in cardDetail){
+        let userprofiles = Profiles.users;
+
+        for(let i in userprofiles){
+
+            let imageUrl = this.safeReturn(userprofiles[i],'imageUrl','');
+            let name = this.safeReturn(userprofiles[i],'name','');
+            let type = this.safeReturn(userprofiles[i],'profession','profession');
+            if(imageUrl == '')
+            {
+                continue;
+            }
             cardXml.push(
-                <div className="card-wrap col-md-4" key={cardDetail[i].name}>
-                    <img src={cardDetail[i].url} className="" />
-                    <h3>{cardDetail[i].name}</h3>
-                    <p className="">{cardDetail[i].type}</p>
+                <div className="card-wrap col-md-4" key={i}>
+                    <img src={imageUrl} className="" />
+                    <h3>{name}</h3>
+                    <p className="">{type}</p>
                 </div>
             )
         }
